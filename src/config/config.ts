b93686fa2963370a6a4cf23b4e2389a5161ec2d2
unknown <facuugerez37@gmail.com>
2024-03-12 21:23:36 -0300

    auth services to-do

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,35 +1,41 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import Joi from 'joi';
+import dotenv from "dotenv";
+import path from "path";
+import Joi from "joi";
 
-dotenv.config({ path: path.join(process.cwd(), '.env') })
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 const envVarsSchema = Joi.object()
   .keys({
-    NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
+    NODE_ENV: Joi.string()
+      .valid("production", "development", "test")
+      .required(),
     PORT: Joi.number().default(3000),
-    SMTP_HOST: Joi.string().description('servidor que va a enviar los emails'),
-    SMTP_PORT: Joi.number().description('puerto que conecta el email server'),
-    SMTP_USERNAME: Joi.string().description('username para email server'),
-    SMTP_PASSWORD: Joi.string().description('password para email server'),
-    EMAIL_FROM: Joi.string().description('emisario que envia el mail desde la app')
+    SMTP_HOST: Joi.string().description("servidor que va a enviar los emails"),
+    SMTP_PORT: Joi.number().description("puerto que conecta el email server"),
+    SMTP_USERNAME: Joi.string().description("username para email server"),
+    SMTP_PASSWORD: Joi.string().description("password para email server"),
+    EMAIL_FROM: Joi.string().description(
+      "emisario que envia el mail desde la app"
+    ),
+  })
+  .unknown();
 
-}).unknown();
-
-const { value: envVars, error} = envVarsSchema.prefs({ errors: { label: 'key'}})
-.validate(process.env);
-
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
 
 export default {
-    env: envVars.NODE_ENV,
-    port: envVars.PORT,
-    email: {
-        host: envVars.SMTP_HOST,
-        port: envVars.SMTP_PORT,
-        auth: {
-            user: envVars.SMTP_USERNAME,
-            pass: envVars.SMTP_PASSWORD,
-        }
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+  email: {
+    smtp: {
+      host: envVars.SMTP_HOST,
+      port: envVars.SMTP_PORT,
+      auth: {
+        user: envVars.SMTP_USERNAME,
+        pass: envVars.SMTP_PASSWORD,
+      },
     },
     from: envVars.EMAIL_FROM,
-}
+  },
+};
---
+++ src/services/auth.service.ts
@@ -0,0 +1,4 @@
+import userService from "./user.service";
+import httpStatus from "http-status";
+import ApiError from "utils/ApiError";
+import emailService from "./email.service";
