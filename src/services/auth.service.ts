afd3e91213a535563885f22f453bae71d7f3d67d
unknown <facuugerez37@gmail.com>
2024-03-15 18:25:24 -0300

    tokens ready

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,9 @@
 import userService from "./user.service";
 import httpStatus from "http-status";
 import ApiError from "utils/ApiError";
-import emailService from "./email.service";
+import tokenService from "./token.service";
+import { TokenType, User } from '@prisma/client';
+import prisma from "client";
+import { encryptPassword, isPasswordMatch } from "utils/encryption";
+import { AuthTokensResponse } from "types/response";
+import exclude from "utils/exclude";
