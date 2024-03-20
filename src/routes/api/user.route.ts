9d1111b2b0447480ae2437b8d73677edf58aa4d3
unknown <facuugerez37@gmail.com>
2024-03-20 15:53:22 -0300

    routes done

--- src/routes/api/user.route.ts
+++ src/routes/api/user.route.ts
@@ -10,3 +10,11 @@ router
 .route('/')
 .post(validate(userValidation.createUser), userController.createUser)
 .get(validate(userValidation.getUsers), userController.getUsers);
+
+router.
+route('/:userId')
+.get(validate(userValidation.getUser), userController.getUser)
+.patch(validate(userValidation.updateUser), userController.updateUser)
+.delete(validate(userValidation.deleteUser), userController.deleteUser);
+
+export default router;
